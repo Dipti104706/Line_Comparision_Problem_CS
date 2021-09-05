9ea37730747633fb37796f53b8dc16df889c4cdf
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-05 16:35:06 +0530

    [Dipti] added welcome note to line comparision problem

---
+++ Line_Comparision_Problem_CS/LineComparision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Line_Comparision_Problem_CS
+{/// <summary>
+/// Template for line comparison problem
+/// </summary>
+    class LineComparision
+    {
+        //Welcome note for line comparision problem
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            Console.ReadLine();
+        }
+    }
+}
