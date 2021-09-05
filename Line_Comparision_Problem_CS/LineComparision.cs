0b97f9d5ceac894212cd2f72b123c02d31f88e0b
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-05 16:53:18 +0530

    [Dipti] added usercase1 to calculate length of a line

--- Line_Comparision_Problem_CS/LineComparision.cs
+++ Line_Comparision_Problem_CS/LineComparision.cs
@@ -6,10 +6,21 @@ namespace Line_Comparision_Problem_CS
 /// </summary>
     class LineComparision
     {
-        //Welcome note for line comparision problem
+        //UC1 for Calculation of length using cartesian system
+        
+        //Creating distance method to calculate length of a line using two points 
+        //two points are (x1,y1) & (x2,y2)
+        static double distance(int x1, int y1, int x2, int y2)
+        {
+            //To evaluate math expression Math method() used 
+            double lineLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            //Round method() used for rounding decimal value up to 2 digit
+            double length = Math.Round(lineLength, 2);
+            return length;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            Console.WriteLine("line length:" + LineComparision.distance(3, 4, 4, 3));
             Console.ReadLine();
         }
     }
